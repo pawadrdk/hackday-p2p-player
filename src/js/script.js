a34ed1aa960a4677761def146a2594d4573885db
Mathias Buus <mathiasbuus@gmail.com>
2017-05-19 02:46:07 +0200

    fix url

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 // hls
 
-const mSeedPeer = "/browser-server/master.m3u8";
+const mSeedPeer = "/browser-server/index_800_av-p.m3u8";
 const hqDR2 = "https://drevent-lh.akamaihd.net/i/event12_0@427365/master.m3u8";
 const DR2 = "https://dr02-lh.akamaihd.net/i/dr02_0@147055/master.m3u8"
 
